4703f8e644bc9ca3e5c79e45e7d288acb6ee748b
rath321 <nikhilrathore329@gmail.com>
2022-02-08 21:37:14 +0530

    "done"

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x48928b6BBf4722EB88c9910871B1ed1cAf543E1b";
 export const contractABI = abi.abi;
